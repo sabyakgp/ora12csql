8bdcc3335e7db26597f40adba263138ec1d9e873
sabyakgp <sabyasachikgp@gmail.com>
2017-12-11 00:34:29 +0530

    remove embsql

--- mpwr/insert.sql
+++
@@ -1,6 +0,0 @@
-delete from embt_fcts_mpwr_excp_gen;
---
-insert into embt_fcts_mpwr_excp_gen
-values('114462077D1', to_date('06/01/2017', 'MM/DD/YYYY'), 54, 44, 5, 98, 'INVALID MEDICARE CLAIM NUMBER');
---
-commit;
