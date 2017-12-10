37ed6553ddd9073db9d0df79c4ec12232c403282
sabyakgp <sabyasachikgp@gmail.com>
2017-12-11 01:06:08 +0530

    change collect.sql

--- collect.sql
+++ collect.sql
@@ -8,19 +8,33 @@ cursor t1 is
 		t1;
 type t1_rec is table of t1%rowtype index by binary_integer;
 
-recs t1_rec;
+type t2_type is RECORD 
+					(
+						colA t2.colA%TYPE,
+						colB t2.colB%TYPE,
+						colC t2.colC%TYPE
+					);
+type t2_rec is table of t2_type	idex by binary_integer;
+
+t1_recs t1_rec;
+t2_recs t2_rec;
 lvDate timestamp(3);
 
 begin
 	open t1;
-	fetch t1 bulk collect into recs;
+	fetch t1 bulk collect into t1_recs;
 	close t1;
 	lvDate := SYSTIMESTAMP;
 
-	forall i in 1..recs.count save exceptions
+	for i in 1..t1_recs.count loop
+		t2_recs.extend;
+		t2_recs(i).colA := t1_recs(i).colA;
+		t2_recs(i)
+
+	forall i in 1..t1_recs.count save exceptions
 		insert 
 			into t2
-		values(recs(i).colA, recs(i).colB, lvDate);
+		values(t1_recs(i).colA, t1_recs(i).colB, lvDate);
 		lvDate := lvDate + INTERVAL '1' SECOND;
 
 commit;
